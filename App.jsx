ec68df77ae9fa24fae3f107e387605de77779562
LeandroMendesgit <124472549+LeandroMendesgit@users.noreply.github.com>
2024-04-18 10:56:04 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack' 
+import ModelScreen from './screens/ModelScreen';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import GeneralQuizScreen from './screens/GeneralQuizScreen';
+import DesenhosScreen from './screens/DesenhosScreen';
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Model' component={ModelScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Sobre' component={AboutScreen} />
+        <Stack.Screen name='GeneralQuiz' component={GeneralQuizScreen} />
+        <Stack.Screen name='Desenhos' component={DesenhosScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
+
